59e4cf97f475a604b7c22029143f0f02c492f1d9
mkrill <michael.krill@telekom.de>
2019-12-10 14:22:02 +0100

    form in index.html in post-action geändert, doctorcontroller aktualisiert

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -2,13 +2,13 @@ package com.example.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DoctorController {
 
-	@GetMapping("/doctor")
+	@PostMapping("/doctor")
 	public String doctor (Model model,
 			@RequestParam(value = "name", required = false, defaultValue = "John Smith") String name,
 			@RequestParam(value = "number", required = false, defaultValue = "0") int number) {
